6e808fe668df2fafea35432189d07685f7b2a30b
malikusman890 <151428179+malikusman890@users.noreply.github.com>
2023-11-19 12:47:34 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923125318340', 'Malik Bot Master', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
